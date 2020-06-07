2b083dc3cdd3856797309e80f6a0c554630f06dd
SergeyBibik1 <captan05@yandex.ru>
2020-06-07 17:22:40 +0300

    first commit

---
+++ widget.cpp
@@ -0,0 +1,374 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+#include "sprite.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    m_col = 40;
+    m_row = 30;
+    m_size_cell = 16;
+    m_width_window = m_col * m_size_cell;
+    m_height_window = m_row * m_size_cell;
+    resize(m_width_window,m_height_window);
+
+    qsrand(QTime(0,0,0).secsTo(QTime::currentTime()));
+
+    m_pixmap_tileset.load(":/images/tileset.png");
+    m_pixmap_tileset.setMask(m_pixmap_tileset.createMaskFromColor(QColor(255,0,255)));
+    m_pixmap_background = m_pixmap_tileset.copy(0,16,640,480);
+
+    m_pixmap_blood.load(":/images/blood.png");
+    m_pixmap_blood.setMask(m_pixmap_blood.createMaskFromColor(QColor(255,255,255)));
+
+    m_pixmap_wall = m_pixmap_tileset.copy(16*8+2,0,16,16);
+
+    GameInit();
+
+    timer_id = startTimer(140);
+    m_test_key = false;
+}
+
+Widget::~Widget()
+{
+    delete ui;
+    killTimer(timer_id);
+    for(int i = 0;i<snake.size();i++)
+    {
+        delete snake[i];
+        snake[i] = nullptr;
+    }
+
+    snake.clear();
+}
+
+void Widget::GameInit()
+{
+    m_test_key = false;
+    m_tl = false;
+    m_tr = false;
+    m_bl = false;
+    m_br = false;
+    m_newItem = false;
+
+    m_pixmap_head = m_pixmap_tileset.copy(0,0,16,16);
+    m_pSprite_head = new Sprite(m_pixmap_head, 1, 180);
+    m_pSprite_head->SetPosition(m_width_window/2, m_height_window/2);
+    m_pSprite_head->SetVelocity(16, 0);
+    snake.push_back(m_pSprite_head);
+
+    m_pixmap_mid_tail = m_pixmap_tileset.copy(16,0,16,16);
+    m_pSprite_tail = new Sprite(m_pixmap_mid_tail, 1);
+    m_pSprite_tail->SetPosition(m_width_window/2 - m_pSprite_tail->GetPosition().width(),
+                               m_height_window/2);
+    m_pSprite_tail->SetVelocity(16, 0);
+    snake.push_back(m_pSprite_tail);
+
+    m_pSprite_tail = new Sprite(m_pixmap_mid_tail, 1);
+    m_pSprite_tail->SetPosition(m_width_window/2 - m_pSprite_tail->GetPosition().width()*2,
+                               m_height_window/2);
+    m_pSprite_tail->SetVelocity(16, 0);
+    snake.push_back(m_pSprite_tail);
+
+    m_pixmap_angle_tail = m_pixmap_tileset.copy(16*2,0,16,16);
+    m_pixmap_end_tail = m_pixmap_tileset.copy(16*3,0,16,16);
+
+    m_pSprite_apple = new Sprite(m_pixmap_tileset.copy(16*5,0,16*3,16), 3);
+    m_pSprite_apple->SetPosition(Random(2, m_col-2)*m_size_cell,
+                                 Random(2, m_row-2)*m_size_cell);
+    m_pSprite_apple->SetVelocity(0, 0);
+
+    m_pSprite_blood = new Sprite(m_pixmap_blood.copy(0,0,40*5,0), 5);
+    m_pSprite_blood->SetVelocity(0, 0);
+
+    score_ = 0;
+}
+
+int Widget::Random(int low, int high)
+{
+    return low + qrand() % ((high + 1) - low);
+}
+
+void Widget::timerEvent(QTimerEvent *e)
+{
+    if(timer_id == e->timerId())
+    {
+        if(m_pause == false)
+        {
+            //змея ползет
+            for(int i = snake.size()-1;i > 0 ;i--)
+            {
+                snake[i]->SetPosition(snake[i-1]->GetPosition());
+                snake[i]->SetVelocity(snake[i-1]->GetVelocity());
+            }
+
+            snake.front()->SetPosition(snake.front()->GetPosition().left() + snake.front()->GetVelocity().x(),
+                    snake.front()->GetPosition().top() + snake.front()->GetVelocity().y());
+
+            m_test_key = true;
+        }
+
+        //змея выходит за пределы игрового поля
+        if(snake.front()->GetPosition().left() < m_size_cell)
+        {
+            snake.front()->SetPosition((m_width_window-m_size_cell) - snake.front()->GetPosition().width(),
+                                       snake.front()->GetPosition().top());
+        }
+        else
+        if(snake.front()->GetPosition().right() > m_width_window-m_size_cell)
+            snake.front()->SetPosition(m_size_cell, snake.front()->GetPosition().top());
+
+        if(snake.front()->GetPosition().bottom() > m_height_window-m_size_cell)
+            snake.front()->SetPosition(snake.front()->GetPosition().left(), m_size_cell);
+        else
+        if(snake.front()->GetPosition().top() < m_size_cell)
+        {
+            snake.front()->SetPosition(snake.front()->GetPosition().left(),
+                                       m_height_window-m_size_cell-snake.front()->GetPosition().height());
+        }
+
+        //змея ест яблоко
+        for(int i = 0; i < snake.size(); i++)
+        {
+            //если яблоко попало на змею, считаем его проглоченым
+            if(m_pSprite_apple->GetPosition().contains(snake[i]->GetPosition()))
+            {
+                //и растет
+                m_newItem = true;
+                m_pSprite_tail = new Sprite(m_pixmap_tileset.copy(16,0,16,16),1);
+                m_pSprite_tail->SetPosition(snake[snake.size()-1]->GetPosition());
+                m_pSprite_tail->SetVelocity(snake[snake.size()-1]->GetVelocity());
+                snake.push_back(m_pSprite_tail);
+
+                m_pSprite_blood->SetPosition(m_pSprite_apple->GetPosition());
+                m_pSprite_blood->SetOneFrame(true);
+
+                score_++;
+
+                //перемещаем яблоко в случайную координату
+                m_pSprite_apple->SetPosition(Random(2, m_col-2)*m_size_cell,
+                                             Random(2, m_row-2)*m_size_cell);
+                break;
+            }
+        }
+
+        //змея кусает свой хвост, умирает...
+        for(int i = 1; i < snake.size(); i++)
+        {
+            if(snake.front()->GetPosition().contains(snake[i]->GetPosition()))
+            {
+               QMessageBox::about(0,"Snake","Game Over");
+
+               for(int i = 0;i<snake.size();i++)
+               {
+                   delete snake[i];
+                   snake[i] = 0;
+               }
+
+               snake.clear();
+               // ...и опять возрождается
+               GameInit();
+            }
+        }
+    }
+    m_pSprite_apple->UpdateFrame();
+    m_pSprite_blood->UpdateFrame();
+    update();
+}
+
+void Widget::keyPressEvent(QKeyEvent *event)
+{
+    switch(event->key())
+    {
+        case Qt::Key_Left:
+        {
+            if((snake.front()->GetVelocity().y() > 0 || snake.front()->GetVelocity().y() < 0) && m_test_key)
+                snake.front()->SetVelocity(-16,0);
+        }break;
+        case Qt::Key_Right:
+        {
+            if((snake.front()->GetVelocity().y() > 0 || snake.front()->GetVelocity().y() < 0) && m_test_key)
+                snake.front()->SetVelocity(16,0);
+        }break;
+        case Qt::Key_Up:
+        {
+            if((snake.front()->GetVelocity().x() > 0 || snake.front()->GetVelocity().x() < 0) && m_test_key)
+                snake.front()->SetVelocity(0,-16);
+        }break;
+        case Qt::Key_Down:
+        {
+            if((snake.front()->GetVelocity().x() > 0 || snake.front()->GetVelocity().x() < 0) && m_test_key)
+                snake.front()->SetVelocity(0,16);
+        }break;
+        case Qt::Key_Space:
+        {
+            m_pause = (m_pause) ? false : true;
+        }break;
+    }
+    m_test_key = false;
+}
+
+void Widget::DrawToTurn(QPainter* painter, int i, int a_end, int a_angle)
+{
+    m_tl = false;
+    m_tr = false;
+    m_bl = false;
+    m_br = false;
+    if(i == snake.size()-1)
+        snake[i]->Draw(painter, m_pixmap_end_tail, a_end);
+    else
+    if(m_newItem)
+    {
+        m_newItem = false;
+        snake[i-1]->Draw(painter, m_pixmap_angle_tail, a_angle);
+    }
+    else
+        snake[i]->Draw(painter, m_pixmap_angle_tail, a_angle);
+}
+
+void Widget::DrawToDir(QPainter* painter, int i, int a_end, int a_mid)
+{
+    if(i == snake.size()-1)
+        snake[i]->Draw(painter, m_pixmap_end_tail, a_end);
+    else
+    if(m_newItem)
+    {
+        m_newItem = false;
+        if(!m_br || !m_tr)
+            snake[i-1]->Draw(painter, m_pixmap_mid_tail, a_mid);
+    }
+    else
+        snake[i]->Draw(painter, m_pixmap_mid_tail, a_mid);
+}
+
+void Widget::paintEvent(QPaintEvent*)
+{
+    QPainter painter(this);
+    //int side = qMin(width(), height());
+    //painter.setViewport((width() - side)/2, (height() - side)/2, side, side);
+    painter.setWindow(0,0,m_width_window, m_height_window);
+    painter.drawPixmap(0,0,m_pixmap_background);
+
+    for(int row=0;row<m_row*m_size_cell;row+=m_size_cell)
+        for(int col=0;col<m_col*m_size_cell;col+=m_size_cell)
+        {
+            if(row == 0 || row == (m_row-1)*m_size_cell ||
+                    col == 0 || col == (m_col-1)*m_size_cell)
+            {
+                if(row==0*m_size_cell && (col>=2*m_size_cell && col<=9*m_size_cell))
+                {
+                    continue;
+                }
+                else
+                    painter.drawPixmap(col,row,m_pixmap_wall);
+            }
+        }
+
+    DrawScore(painter);
+
+    if(m_pause == false)
+    {
+        m_pSprite_apple->Draw(&painter);
+
+        if(m_pSprite_blood->GetOneFrame())
+        {
+            m_pSprite_blood->Draw(&painter);
+            m_pSprite_blood->SetOneFrame(false);
+        }
+    }
+    else
+    {
+        DrawTextPause(painter);
+    }
+
+    if(m_pause == false)
+    {
+        //head
+        if(snake.front()->GetVelocity() == QPoint(0,-16))
+            snake.front()->Draw(&painter, m_pixmap_head, 90);
+        else
+        if(snake.front()->GetVelocity() == QPoint(0,16))
+            snake.front()->Draw(&painter, m_pixmap_head, -90);
+        else
+        if(snake.front()->GetVelocity() == QPoint(-16,0))
+            snake.front()->Draw(&painter, m_pixmap_head, 0);
+        else
+        if(snake.front()->GetVelocity() == QPoint(16,0))
+            snake.front()->Draw(&painter, m_pixmap_head, 180);
+
+
+        for(int i = snake.size()-1;i>0;--i)
+        {
+            if(snake[i]->GetVelocity() == QPoint(-16,0))
+            {
+                if(m_tl) DrawToTurn(&painter, i, 90,-90);
+                else
+                if(m_bl) DrawToTurn(&painter, i,90,0);
+                else DrawToDir(&painter, i,90,0);
+
+                m_br = true;
+                m_tr = true;
+            }
+            else
+            if(snake[i]->GetVelocity() == QPoint(16,0))
+            {
+                if(m_tr) DrawToTurn(&painter, i, -90, 180);
+                else
+                if(m_br) DrawToTurn(&painter, i, -90, 90);
+                else DrawToDir(&painter, i,-90,0);
+
+                m_bl = true;
+                m_tl = true;
+            }
+            else
+            if(snake[i]->GetVelocity() == QPoint(0,16))
+            {
+                if(m_tr) DrawToTurn(&painter, i,0, -180);
+                else
+                if(m_tl) DrawToTurn(&painter, i, 0,-90);
+                else DrawToDir(&painter, i,0,90);
+
+                m_bl = true;
+                m_br = true;
+            }
+            else
+            if(snake[i]->GetVelocity() == QPoint(0,-16))
+            {
+                if(m_br) DrawToTurn(&painter, i,180,90);
+                else
+                if(m_bl) DrawToTurn(&painter, i,180,0);
+                else DrawToDir(&painter, i,180,90);
+
+                m_tl = true;
+                m_tr = true;
+            }
+        }
+    }
+}
+
+void Widget::DrawTextPause(QPainter &p)
+{
+    QFont font;
+    font.setPixelSize(40);
+    p.setFont(font);
+    int width_text = 350;
+    int height_text = 100;
+    QRect rect;
+    rect.setX(640/2-width_text/2);
+    rect.setY(480/2-height_text/2);
+    rect.setWidth(width_text);
+    rect.setHeight(height_text);
+    p.drawText(rect, Qt::AlignCenter,"P A U S E\npress space to play");
+}
+
+void Widget::DrawScore(QPainter& p)
+{
+    QFont font;
+    font.setPixelSize(16);
+    font.setBold(true);
+    p.setFont(font);
+    p.drawText(16*2,0,250,16,Qt::TextSingleLine,"S C O R E: " + QString::number(score_));
+}
---
+++ widget.h
@@ -0,0 +1,73 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QBitmap>
+#include <QKeyEvent>
+#include <QVector>
+
+namespace Ui {
+class Widget;
+}
+
+class Sprite;
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+protected:
+    void paintEvent(QPaintEvent *);
+    void timerEvent(QTimerEvent *);
+    void keyPressEvent(QKeyEvent *);
+
+private:
+    void GameInit();
+    void DrawToTurn(QPainter* painter, int i, int a_end, int a_angle);
+    void DrawToDir(QPainter* painter, int i, int a_end, int a_mid);
+    void DrawTextPause(QPainter &p);
+    int Random(int low, int high);
+    void DrawScore(QPainter& p);
+
+private:
+    Ui::Widget *ui;
+    int m_width_window;
+    int m_height_window;
+    int m_col;
+    int m_row;
+    int m_size_cell;
+
+    QPixmap m_pixmap_tileset;
+    QPixmap m_pixmap_background;
+
+    QPixmap m_pixmap_head;
+    QPixmap m_pixmap_mid_tail;
+    QPixmap m_pixmap_angle_tail;
+    QPixmap m_pixmap_end_tail;
+    QPixmap m_pixmap_blood;
+    QPixmap m_pixmap_wall;
+
+    Sprite* m_pSprite_head;
+    Sprite* m_pSprite_tail;
+    Sprite* m_pSprite_apple;
+    Sprite* m_pSprite_blood;
+
+    QVector<Sprite*> snake;
+
+    int timer_id;
+    bool m_test_key;
+
+    bool m_tl;
+    bool m_tr;
+    bool m_bl;
+    bool m_br;
+
+    bool m_newItem;
+    bool m_pause;
+    unsigned int score_;
+};
+
+#endif // WIDGET_H
